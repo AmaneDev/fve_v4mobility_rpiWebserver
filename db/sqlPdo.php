3e3eba27a9bd454b190960c964a8444d314a24bd
AmaneDev <michal.polynek5@gmail.com>
2025-04-10 13:29:30 +0200

    konfigarace DB path v config.inc

--- db/sqlPdo.php
+++ db/sqlPdo.php
@@ -1,12 +1,15 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT']."/config.inc";
 //konfigurační soubor kde vytváříme objekt $pdo pro připojení k databází - inicializujeme typ db(sqlite) a cestu k souboru
 
 
 class DatabaseConnection {
     private $pdo;
-    private $dbFile = __DIR__ . '/../sensordata.db';
+    private $dbFile;
 
     public function __construct() {
+        global $dbPath; // Přístup k proměnné $dbPath definované v config.inc
+        $this->dbFile = $_SERVER['DOCUMENT_ROOT'] . $dbPath;
         try {
             $this->pdo = new PDO('sqlite:' . $this->dbFile);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
